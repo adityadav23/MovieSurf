1fefe81d928a9a18dfb5bfc08d1dfb7c4e00681e
Aditya Yadav <aditya.yadav@dhwaniris.comm>
2024-04-29 01:09:23 +0530

    refs to select dom element

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Children, useEffect, useState } from "react";
+import { Children, useEffect, useRef, useState } from "react";
 import StarRating from "./StarRating";
 const tempMovieData = [
   {
@@ -440,11 +440,28 @@ function Logo(){
 }
 
 function Search({query, setQuery}){
+  const inputEl = useRef(null)
+
+  useEffect(function(){
+    function callback(e){
+      if(document.activeElement === inputEl.current){
+        return;
+      }
+      if(e.code === "Enter"){
+        inputEl.current.focus();
+        setQuery("");
+      }
+    }
+    document.addEventListener('keydown',callback)
+    return ()=> document.addEventListener('keydown', callback)
+  },[setQuery]);
+
   return ( <input
     className="search"
     type="text"
     placeholder="Search movies..."
     value={query}
     onChange={(e) => setQuery(e.target.value)}
+    ref={inputEl}
   />)
 }
